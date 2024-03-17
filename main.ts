80f1641311604189a9c7f646bc114bf8325d4768
Antonio Contreras <drancope@gmail.com>
2024-03-17 20:36:08 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+let temperatura = 0
+basic.forever(function () {
+    temperatura = input.temperature()
+    if (true) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
